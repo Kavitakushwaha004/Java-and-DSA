503c9a75aec0541e0ad22fc12502a4545f47bdcd
Kavitakushwaha004 <sakshikushwaha8823@gmail.comgit config --listgit config --global user.email sakshikushwaha8823@gmail.comgit config --list git config --global user.email sakshikushwaha8823@gmail.comgit config --list  git config --global user.email sakshikushwaha8823@gmail.comgit config --global user.email sakshikushwaha8823@gmail.com>
2025-12-24 15:56:14 -0800

    Merge branch 'master' of https://github.com/Kavitakushwaha004/Java-and-DSA # Please enter a commit message to explain why this merge is necessary, # especially if it merges an updated upstream into a topic branch. # # Lines starting with '#' will be ignored, and an empty message aborts # the commit.wq Add DataTypes.java CheckPrime

---
+++ src/CheckPrime.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+class CheckPrime {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number:");
+        int n = sc.nextInt();
+        if (n == 2) {
+            System.out.println(" prime");
+        } else {
+            boolean isPrime = true;
+
+            for(int i = 2; i <= n - 1; ++i) {
+                if (n % i == 0) {
+                    isPrime = false;
+                }
+            }
+
+            if (isPrime) {
+                System.out.println("prime");
+            } else {
+                System.out.println("not prime");
+            }
+        }
+
+    }
+}
