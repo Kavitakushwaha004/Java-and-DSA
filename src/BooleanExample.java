9503d369f434ed507ccc9cbf12ad7215a8cd2645
Kavitakushwaha004 <sakshikushwaha8823@gmail.comgit config --listgit config --global user.email sakshikushwaha8823@gmail.comgit config --list git config --global user.email sakshikushwaha8823@gmail.comgit config --list  git config --global user.email sakshikushwaha8823@gmail.comgit config --global user.email sakshikushwaha8823@gmail.com>
2025-12-24 14:45:07 -0800

    Single file commit

---
+++ src/BooleanExample.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class BooleanExample{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        boolean isevening =true;
+        if (isevening ) {
+            System.out.print("Enjoy the sunshines....");
+        } else {
+            System.out.print("stay at home...");
+        }
+    }
+}
