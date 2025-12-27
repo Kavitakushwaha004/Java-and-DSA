323c4bc44c390bb3c19506e3037d131b0a81fc7e
Kavitakushwaha004 <sakshikushwaha8823@gmail.comgit config --listgit config --global user.email sakshikushwaha8823@gmail.comgit config --list git config --global user.email sakshikushwaha8823@gmail.comgit config --list  git config --global user.email sakshikushwaha8823@gmail.comgit config --global user.email sakshikushwaha8823@gmail.com>
2025-12-27 14:46:42 -0800

    InvertedHalfPyramid

---
+++ src/InvertedHalfPyramid.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class InvertedHalfPyramid{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter n:");
+        int n=sc.nextInt();
+        for(int i=1; i<=n; i++){
+            for(int j=n; j>=i; j--){
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+}
