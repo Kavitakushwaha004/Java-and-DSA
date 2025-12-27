c833377a2049e0ceb3ebf95a80be613e6e8a2892
kavita kushwaha <sakshikushwaha8823@gmail.com>
2025-12-27 02:13:11 -0800

    Delete src/TypeCasting.java

--- src/TypeCasting.java
+++
@@ -1,9 +0,0 @@
-
-class TypeCasting{
-    public static void main(String[] args){
-     float f=1.2f;
-     System.out.println("Float value is   "+f);
-     int i=(int) f;
-     System.out.println("Integer value is   "+i);
-    }
-}
