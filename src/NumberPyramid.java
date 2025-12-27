2aa2b0b2238a92d7516177b798d9bf06da0745c8
Kavitakushwaha004 <sakshikushwaha8823@gmail.comgit config --listgit config --global user.email sakshikushwaha8823@gmail.comgit config --list git config --global user.email sakshikushwaha8823@gmail.comgit config --list  git config --global user.email sakshikushwaha8823@gmail.comgit config --global user.email sakshikushwaha8823@gmail.com>
2025-12-27 14:48:50 -0800

    NumberPyramid

---
+++ src/NumberPyramid.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class NumberPyramid{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter n:");
+        int n=sc.nextInt();
+        for(int i=1; i<=n; i++){
+            for(int j=n; j>=i; j--){
+                System.out.print(" ");
+            }
+            for(int k=1; k<=i; k++){
+                System.out.print(" "+i);
+            }
+            System.out.println();
+        }
+    }
+}
