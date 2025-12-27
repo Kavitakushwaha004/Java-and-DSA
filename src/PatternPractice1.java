75023333b8a0f2b3b4c8a4cd90348115f7ea859c
Kavitakushwaha004 <sakshikushwaha8823@gmail.comgit config --listgit config --global user.email sakshikushwaha8823@gmail.comgit config --list git config --global user.email sakshikushwaha8823@gmail.comgit config --list  git config --global user.email sakshikushwaha8823@gmail.comgit config --global user.email sakshikushwaha8823@gmail.com>
2025-12-27 14:49:34 -0800

    PatternPractice1

---
+++ src/PatternPractice1.java
@@ -0,0 +1,37 @@
+/*import java .util.*;
+class PatternPractice1{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter n:");
+        int n=sc.nextInt();
+        int num=1;
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print( num+" " );
+                num++;
+            }
+            System.out.println();
+        }
+    }
+}*/
+
+//print 010101
+import java.util.*;
+class PatternPractice1{
+   public static void main(String[] args){
+       Scanner sc=new Scanner(System.in);
+       System.out.print("Enter n:");
+       int n=sc.nextInt();
+       for(int i=1; i<=n; i++){
+           for(int j=1; j<=i; j++){
+               int sum=i+j;
+               if(sum%2==0) {
+                   System.out.print("1"+" ");
+               }else{
+                   System.out.print("0"+" ");
+               }
+           }
+           System.out.println();
+       }
+   }
+}
