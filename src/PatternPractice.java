d742bd1e451859889a1bbf5389daffc0f580b9ef
Kavitakushwaha004 <sakshikushwaha8823@gmail.comgit config --listgit config --global user.email sakshikushwaha8823@gmail.comgit config --list git config --global user.email sakshikushwaha8823@gmail.comgit config --list  git config --global user.email sakshikushwaha8823@gmail.comgit config --global user.email sakshikushwaha8823@gmail.com>
2025-12-27 14:49:19 -0800

    PatternPractice

---
+++ src/PatternPractice.java
@@ -0,0 +1,47 @@
+import java.util.*;/*
+class PattrenPractice{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter n:");
+        int n=sc.nextInt();
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}*/
+//
+/*
+import java.util.*;
+class PatternPractice{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter n:");
+        int n=sc.nextInt ();
+        for(int i=n; i>=1; i--){
+            for(int j=1; j<=i; j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}*/
+import java.util.*;
+class PattrenPractice{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter n:");
+        int n=sc.nextInt();
+        for(int i=1; i<=n; i++){
+            for(int j=n; j>=i; j--){
+                System.out.print(" ");
+            }
+            for(int j=1; j<=i; j++){
+                System.out.print(" " +j);
+            }
+            System.out.println();
+        }
+    }
+}
